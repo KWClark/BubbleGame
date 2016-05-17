845b891560fa6a58ab250c7783e5be0ed6f634f0
Kevin Clark <Kevin Clark>
2016-05-16 22:25:51 -0600

    Add subtraction as an option

--- src/BubbleGame.java
+++ src/BubbleGame.java
@@ -13,15 +13,30 @@ public class BubbleGame {
 
         int num;
         int numTwo;
+        String response;
 
-        System.out.println("Please enter two whole numbers.");
-        num = scan.nextInt();
-        numTwo = scan.nextInt();
+        System.out.println("Would you like to add or subtract two numbers?");
+        System.out.println("Please type 'add' or 'sub'.");
+        response = scan.next();
 
+        if (response.equals("add")){
 
-        int numbers = addNumbers(num, numTwo);
-        System.out.println(numbers);
-        System.out.println("Congrats! You added some numbers.");
+            System.out.println("Please enter two whole numbers.");
+            num = scan.nextInt();
+            numTwo = scan.nextInt();
+            int numbers = addNumbers(num, numTwo);
+            System.out.println(numbers);
+            System.out.println("Congrats! You added some numbers.");
+        }
+        else {
+            System.out.println("Please enter two whole numbers.");
+            num = scan.nextInt();
+            numTwo = scan.nextInt();
+            int numbers = subtNumbers(num, numTwo);
+            System.out.println(numbers);
+            System.out.println("Congrats! You subtracted some numbers.");
+        }
+
 
     }
 
@@ -29,5 +44,9 @@ public class BubbleGame {
         int answer;
         answer = num1 + num2;
         return answer;
+    }
+
+    static int subtNumbers( int num1, int num2) {
+        return num1 - num2;
     }
 }
